8b7bda646a21df564be249de603b6702f030388b
Daniel <danielwu116@gmail.com>
2021-03-24 14:16:58 -0400

    Add import_db.sql

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS question_follows;
-DROP TABLE
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 PRAGMA foreign_keys = ON;
 
@@ -7,7 +10,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL,
-)
+);
 
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
@@ -16,7 +19,7 @@ CREATE TABLE questions (
     user_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
@@ -25,20 +28,20 @@ CREATE TABLE question_follows (
 
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
-    parent_reply_id INTEGER NOT NULL,
+    parent_reply_id INTEGER ,
     user_id INTEGER NOT NULL,
     body TEXT NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
@@ -47,7 +50,33 @@ CREATE TABLE question_likes (
 
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(question_id) REFERENCES questions(id)
-)
+);
 
+INSERT INTO 
+    users (fname, lname)
+VALUES 
+    ('Angela', 'Turi'), ('Daniel', 'Wu');
 
+INSERT INTO
+    questions (title, body, user_id)
+VALUES
+    ('Angela''s Question', 'How do you query in an RDBMS?', (SELECT id FROM users WHERE fname = 'Angela' AND lname = 'Turi')),
+    ('Daniel''s Question', 'What is a heredoc?', (SELECT id FROM users WHERE fname = 'Daniel' AND lname = 'Wu'));
 
+INSERT INTO 
+    question_follows (user_id, question_id)
+VALUES
+    ((SELECT id FROM users WHERE fname = 'Angela' AND lname = 'Turi'), (SELECT id FROM questions WHERE title = 'Angela''s Question')),
+    ((SELECT id FROM users WHERE fname = 'Daniel' AND lname = 'Wu'), (SELECT id FROM questions WHERE title = 'Daniel''s Question'));
+
+INSERT INTO 
+    replies (question_id, parent_reply_id, user_id, body)
+VALUES 
+    ((SELECT id FROM questions WHERE title = 'Angela''s Question'), NULL, (SELECT id FROM users WHERE fname = 'Angela' AND lname = 'Turi'), 'Ask the TAs for help'),
+    ((SELECT id FROM questions WHERE title = 'Daniel''s Question'), (SELECT id FROM replies WHERE body = 'Ask the TAs for help'), (SELECT id FROM users WHERE fname = 'Daniel' AND lname = 'Wu'), 'Ask the circle leader for help');
+
+INSERT INTO 
+    question_likes (user_id, question_id)
+VALUES
+    ((SELECT id FROM users WHERE fname = 'Angela' AND lname = 'Turi'), (SELECT id FROM questions WHERE title = 'Angela''s Question')),
+    ((SELECT id FROM users WHERE fname = 'Daniel' AND lname = 'Wu'), (SELECT id FROM questions WHERE title = 'Daniel''s Question'));
